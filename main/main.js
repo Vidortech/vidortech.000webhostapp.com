deb76be5f3aa35ed65094268453fbf70c790b8ef
Matchzin <matheusvidordeoliveira@gmail.com>
2024-06-18 17:44:30 -0300

    Comitando mudanças da adição e edição do header nas subpáginas.

--- main/main.js
+++ main/main.js
@@ -1,10 +1,9 @@
 const menuBtn = document.getElementById('menu-btn');
 const menu = document.querySelector('.menu');
-const menuVisibleClass = 'menu-visible'; // Classe que controla a visibilidade do menu
+const menuVisibleClass = 'menu-visible'; 
 
 menuBtn.addEventListener('click', toggleMenu);
 
-// Função para abrir/fechar o menu
 function toggleMenu() {
     if (menu.classList.contains(menuVisibleClass)) {
         closeMenu();
@@ -13,21 +12,16 @@ function toggleMenu() {
     }
 }
 
-// Função para abrir o menu
 function openMenu() {
     menu.classList.add(menuVisibleClass);
-    // Adiciona ouvinte de eventos para fechar o menu ao clicar fora dele
     document.addEventListener('click', closeMenuOnClickOutside);
 }
 
-// Função para fechar o menu
 function closeMenu() {
     menu.classList.remove(menuVisibleClass);
-    // Remove o ouvinte de eventos para fechar o menu ao clicar fora dele
     document.removeEventListener('click', closeMenuOnClickOutside);
 }
 
-// Função para fechar o menu ao clicar fora dele
 function closeMenuOnClickOutside(event) {
     if (!menu.contains(event.target) && event.target !== menuBtn) {
         closeMenu();
